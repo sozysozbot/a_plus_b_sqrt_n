2ddd753457da4416ccc13f349da6d30482c0add3
hsjoihs | sozysozbot <jekto.vatimeliju@gmail.com>
2023-12-18 01:48:38 +0900

    Change the data structure; implement `is_rational`

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,62 @@
 #![warn(clippy::pedantic)]
-use fraction::{prelude::BigFraction, Integer, Zero};
+use fraction::{prelude::BigFraction, GenericFraction, Integer, Zero};
 use num_bigint::BigUint;
+use num_traits::One;
 use std::ops;
 
 #[derive(Debug, Clone)]
-pub struct APlusBSqrtQ {
-    a: BigFraction,
-    b: BigFraction,
-    q: BigUint, // either 0 or non-perfect-square
+pub enum APlusBSqrtQ {
+    Rational(BigFraction),
+    Irrational {
+        a: BigFraction,
+        b: BigFraction,
+        q: BigUint, // non-perfect-square
+    },
 }
 
-pub fn is_perfect_square(n: &BigUint) -> bool {
+#[must_use]
+fn is_perfect_square(n: &BigUint) -> bool {
     let sqrt_n_floor = n.sqrt();
     &(sqrt_n_floor.clone() * sqrt_n_floor) == n
 }
 
 impl APlusBSqrtQ {
+    #[must_use]
     pub fn new(a: BigFraction, b: BigFraction, q: BigUint) -> Self {
         if b == BigFraction::zero() || is_perfect_square(&q) {
-            Self {
-                a: a + b * q.sqrt(),
-                b: BigFraction::zero(),
-                q: BigUint::zero(),
-            }
+            Self::Rational(a + b * q.sqrt())
         } else {
-            Self { a, b, q }
+            Self::Irrational { a, b, q }
         }
+    }
+
+    #[must_use]
+    pub const fn is_rational(&self) -> bool {
+        matches!(self, APlusBSqrtQ::Rational(_))
     }
 }
 
 impl PartialEq for APlusBSqrtQ {
     fn eq(&self, other: &Self) -> bool {
-        match (self.q == BigUint::zero(), other.q == BigUint::zero()) {
-            (true, true) => self.a == other.a,
-            (true, false) | (false, true) => false,
-            (false, false) => {
-                self.a == other.a
-                    && (self.b.clone() * self.b.clone() * self.q.clone()
-                        == other.b.clone() * other.b.clone() * other.q.clone())
+        match (self, other) {
+            (APlusBSqrtQ::Rational(a1), APlusBSqrtQ::Rational(a2)) => a1 == a2,
+            (APlusBSqrtQ::Rational(_), APlusBSqrtQ::Irrational { .. })
+            | (APlusBSqrtQ::Irrational { .. }, APlusBSqrtQ::Rational(_)) => false,
+            (
+                APlusBSqrtQ::Irrational {
+                    a: a1,
+                    b: b1,
+                    q: q1,
+                },
+                APlusBSqrtQ::Irrational {
+                    a: a2,
+                    b: b2,
+                    q: q2,
+                },
+            ) => {
+                a1 == a2
+                    && (b1.clone() * b1.clone() * q1.clone()
+                        == b2.clone() * b2.clone() * q2.clone())
             }
         }
     }
@@ -47,37 +66,57 @@ impl ops::Neg for APlusBSqrtQ {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
-        Self::new(-self.a, -self.b, self.q)
+        match self {
+            APlusBSqrtQ::Rational(a) => Self::Rational(-a),
+            APlusBSqrtQ::Irrational { a, b, q } => Self::new(-a, -b, q),
+        }
     }
 }
 
 impl ops::Add for APlusBSqrtQ {
     type Output = Self;
     fn add(self, other: Self) -> Self::Output {
-        if other.q == BigUint::zero() {
-            Self::new(self.a + other.a, self.b, self.q)
-        } else if self.q == BigUint::zero() {
-            other + self
-        } else if is_perfect_square(&(self.q.clone() * other.q.clone())) {
-            // ans = a1 + b1 sqrt(q1) + a2 + b2 sqrt(q2)
-            // Let q1 = s * s * q
-            // Let q2 = t * t * q
-            // Then q3 = gcd(q1, q2) = u * u * q
-            let q3 = self.q.gcd(&other.q);
-            let s_over_u_squared = self.q / q3.clone();
-            let t_over_u_squared = other.q / q3.clone();
-            assert!(is_perfect_square(&s_over_u_squared));
-            assert!(is_perfect_square(&t_over_u_squared));
-            let s_over_u = s_over_u_squared.sqrt();
-            let t_over_u = t_over_u_squared.sqrt();
+        match (self, other) {
+            (APlusBSqrtQ::Rational(a1), APlusBSqrtQ::Rational(a2)) => Self::Rational(a1 + a2),
+            (APlusBSqrtQ::Irrational { a, b, q }, APlusBSqrtQ::Rational(a2))
+            | (APlusBSqrtQ::Rational(a2), APlusBSqrtQ::Irrational { a, b, q }) => {
+                Self::new(a + a2, b, q)
+            }
 
-            // ans = (a1 + a2) + [b1 * (s/u) + b2 * (t/u)] sqrt(q3)
-            Self::new(self.a + other.a, self.b * s_over_u + other.b * t_over_u, q3)
-        } else {
-            panic!(
-                "Cannot add: sqrt({}) and sqrt({}) are incompatible",
-                &self.q, &other.q
-            )
+            (
+                APlusBSqrtQ::Irrational {
+                    a: a1,
+                    b: b1,
+                    q: q1,
+                },
+                APlusBSqrtQ::Irrational {
+                    a: a2,
+                    b: b2,
+                    q: q2,
+                },
+            ) => {
+                if is_perfect_square(&(q1.clone() * q2.clone())) {
+                    // ans = a1 + b1 sqrt(q1) + a2 + b2 sqrt(q2)
+                    // Let q1 = s * s * q
+                    // Let q2 = t * t * q
+                    // Then q3 = gcd(q1, q2) = u * u * q
+                    let q3 = q1.gcd(&q2);
+                    let s_over_u_squared = q1 / q3.clone();
+                    let t_over_u_squared = q2 / q3.clone();
+                    assert!(is_perfect_square(&s_over_u_squared));
+                    assert!(is_perfect_square(&t_over_u_squared));
+                    let s_over_u = s_over_u_squared.sqrt();
+                    let t_over_u = t_over_u_squared.sqrt();
+
+                    // ans = (a1 + a2) + [b1 * (s/u) + b2 * (t/u)] sqrt(q3)
+                    Self::new(a1 + a2, b1 * s_over_u + b2 * t_over_u, q3)
+                } else {
+                    panic!(
+                        "Cannot add: sqrt({}) and sqrt({}) are incompatible",
+                        &q1, &q2
+                    )
+                }
+            }
         }
     }
 }
@@ -94,63 +133,71 @@ impl ops::Mul for APlusBSqrtQ {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        if rhs.q == BigUint::zero() {
-            let multiplier = rhs.a;
-            Self::new(self.a * multiplier.clone(), self.b * multiplier, self.q)
-        } else if self.q == BigUint::zero() {
-            rhs * self
-        } else if is_perfect_square(&(self.q.clone() * rhs.q.clone())) {
-            // ans = [a1 + b1 sqrt(q1)] [a2 + b2 sqrt(q2)]
-            // Let q1 = s * s * q
-            // Let q2 = t * t * q
-            // Then q3 = gcd(q1, q2) = u * u * q
-            let q3 = self.q.gcd(&rhs.q);
-            let s_over_u_squared = self.q / q3.clone();
-            let t_over_u_squared = rhs.q / q3.clone();
-            assert!(is_perfect_square(&s_over_u_squared));
-            assert!(is_perfect_square(&t_over_u_squared));
-            let s_over_u = s_over_u_squared.sqrt();
-            let t_over_u = t_over_u_squared.sqrt();
+        match (self, rhs) {
+            (APlusBSqrtQ::Rational(a1), APlusBSqrtQ::Rational(a2)) => Self::Rational(a1 * a2),
+            (APlusBSqrtQ::Rational(multiplier), APlusBSqrtQ::Irrational { a, b, q })
+            | (APlusBSqrtQ::Irrational { a, b, q }, APlusBSqrtQ::Rational(multiplier)) => {
+                Self::new(a * multiplier.clone(), b * multiplier, q)
+            }
+            (
+                APlusBSqrtQ::Irrational {
+                    a: a1,
+                    b: b1,
+                    q: q1,
+                },
+                APlusBSqrtQ::Irrational {
+                    a: a2,
+                    b: b2,
+                    q: q2,
+                },
+            ) => {
+                if is_perfect_square(&(q1.clone() * q2.clone())) {
+                    // ans = [a1 + b1 sqrt(q1)] [a2 + b2 sqrt(q2)]
+                    // Let q1 = s * s * q
+                    // Let q2 = t * t * q
+                    // Then q3 = gcd(q1, q2) = u * u * q
+                    let q3 = q1.gcd(&q2);
+                    let s_over_u_squared = q1 / q3.clone();
+                    let t_over_u_squared = q2 / q3.clone();
+                    assert!(is_perfect_square(&s_over_u_squared));
+                    assert!(is_perfect_square(&t_over_u_squared));
+                    let s_over_u = s_over_u_squared.sqrt();
+                    let t_over_u = t_over_u_squared.sqrt();
 
-            // ans = [a1 + b1 * s/u * sqrt(q3)] [a2 + b2 * t/u * sqrt(q3)]
-            //     = [a1*a2   +   b1 * s/u * q3 * b2 * t/u] + [a1 * b2 * t/u   +   a2 * b1 * s/u] sqrt(q3)
-            Self::new(
-                self.a.clone() * rhs.a.clone()
-                    + self.b.clone()
-                        * rhs.b.clone()
-                        * s_over_u.clone()
-                        * q3.clone()
-                        * t_over_u.clone(),
-                self.a * rhs.b * t_over_u + rhs.a * self.b * s_over_u,
-                q3,
-            )
-        } else {
-            panic!(
-                "Cannot add: sqrt({}) and sqrt({}) are incompatible",
-                &self.q, &rhs.q
-            )
+                    // ans = [a1 + b1 * s/u * sqrt(q3)] [a2 + b2 * t/u * sqrt(q3)]
+                    //     = [a1*a2   +   b1 * s/u * q3 * b2 * t/u] + [a1 * b2 * t/u   +   a2 * b1 * s/u] sqrt(q3)
+                    Self::new(
+                        a1.clone() * a2.clone()
+                            + b1.clone()
+                                * b2.clone()
+                                * s_over_u.clone()
+                                * q3.clone()
+                                * t_over_u.clone(),
+                        a1 * b2 * t_over_u + a2 * b1 * s_over_u,
+                        q3,
+                    )
+                } else {
+                    panic!(
+                        "Cannot add: sqrt({}) and sqrt({}) are incompatible",
+                        &q1, &q2
+                    )
+                }
+            }
         }
     }
 }
 
 impl APlusBSqrtQ {
+    #[must_use]
     pub fn recip(self) -> Self {
-        if self.q == BigUint::zero() {
-            Self::new(
-                BigFraction::new(1u8, 1u8) / self.a,
-                BigFraction::zero(),
-                BigUint::zero(),
-            )
-        } else {
-            // ans = 1 / [a + b * sqrt(q)]
-            //     = [a - b * sqrt(q)] / [a^2 - b^2 * q]
-            let dividing_factor =
-                self.a.clone() * self.a.clone() - self.b.clone() * self.b.clone() * self.q.clone();
-            Self::new(
-                self.a / dividing_factor.clone(),
-                -self.b / dividing_factor,
-                self.q,
-            )
+        match self {
+            APlusBSqrtQ::Rational(a) => Self::Rational(GenericFraction::one() / a),
+            APlusBSqrtQ::Irrational { a, b, q } => {
+                // ans = 1 / [a + b * sqrt(q)]
+                //     = [a - b * sqrt(q)] / [a^2 - b^2 * q]
+                let dividing_factor = a.clone() * a.clone() - b.clone() * b.clone() * q.clone();
+                Self::new(a / dividing_factor.clone(), -b / dividing_factor, q)
+            }
         }
     }
 }
@@ -158,36 +205,41 @@ impl APlusBSqrtQ {
 impl ops::Div for APlusBSqrtQ {
     type Output = Self;
 
+    #[allow(clippy::suspicious_arithmetic_impl)]
     fn div(self, rhs: Self) -> Self::Output {
         self * rhs.recip()
     }
 }
 
 impl APlusBSqrtQ {
+    #[must_use]
     pub fn is_positive(&self) -> bool {
-        if self.q == BigUint::zero() {
-            self.a > BigFraction::zero()
-        } else if self.b > BigFraction::zero() {
-            if self.a >= BigFraction::zero() {
-                return true;
+        match self {
+            APlusBSqrtQ::Rational(a) => a > &BigFraction::zero(),
+            APlusBSqrtQ::Irrational { a, b, q } => {
+                if b > &BigFraction::zero() {
+                    if a >= &BigFraction::zero() {
+                        return true;
+                    }
+                    // We know that -a > 0
+                    // We want to check whether b * sqrt(q) > -a > 0
+                    b.clone() * b.clone() * q.clone() > a.clone() * a.clone()
+                } else {
+                    // b is assumed to be non-zero
+                    // Thus, we know that b < 0
+
+                    // We know that (-b) * sqrt(q) > 0
+                    // We want to check whether a + b * sqrt(q) > 0
+                    // Thus, we want to know whether a > (-b) * sqrt(q) > 0
+
+                    // We first need to verify that a is indeed greater than 0
+                    if a <= &BigFraction::zero() {
+                        return false;
+                    }
+
+                    a.clone() * a.clone() > b.clone() * b.clone() * q.clone()
+                }
             }
-            // We know that -a > 0
-            // We want to check whether b * sqrt(q) > -a > 0
-            self.b.clone() * self.b.clone() * self.q.clone() > self.a.clone() * self.a.clone()
-        } else {
-            // self.b is assumed to be non-zero
-            // Thus, we know that self.b < 0
-
-            // We know that (-b) * sqrt(q) > 0
-            // We want to check whether a + b * sqrt(q) > 0
-            // Thus, we want to know whether a > (-b) * sqrt(q) > 0
-
-            // We first need to verify that a is indeed greater than 0
-            if self.a <= BigFraction::zero() {
-                return false;
-            }
-
-            self.a.clone() * self.a.clone() >  self.b.clone() * self.b.clone() * self.q.clone()
         }
     }
 }
@@ -208,7 +260,7 @@ fn eq() {
         BigFraction::new(1u8, 2u8),
         BigUint::new(vec![8u32]),
     );
-    assert_eq!(silver, silver2)
+    assert_eq!(silver, silver2);
 }
 
 #[test]
@@ -228,7 +280,7 @@ fn addition() {
             BigFraction::new(2u8, 1u8),
             BigUint::new(vec![2u32]),
         )
-    )
+    );
 }
 
 #[test]
@@ -242,7 +294,7 @@ fn is_positive() {
         BigUint::new(vec![195u32]),
     );
 
-    assert!(fourteen_minus_root_195.is_positive())
+    assert!(fourteen_minus_root_195.is_positive());
 }
 
 #[test]
@@ -262,7 +314,7 @@ fn multiplication() {
             BigFraction::new(2u8, 1u8),
             BigUint::zero(),
         )
-    )
+    );
 }
 
 #[test]
@@ -282,5 +334,5 @@ fn dividing_gives_unity() {
             BigFraction::new(1u8, 1u8),
             BigUint::zero()
         )
-    )
+    );
 }
