4da67255b1486eda31291a770fc7ddbd618edf61
hsjoihs | sozysozbot <jekto.vatimeliju@gmail.com>
2023-12-20 01:25:00 +0900

    Correctly encapsulate the fields
    Also explains the operations supported by this library

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # APlusBSqrtN
 
 Data type for $a + b \sqrt{n} \ (a, b \in \mathbb{Q}, n \in \mathbb{N}_{\ge 0})$, implemented in Rust.
+
+## Operations supported by this library
+
+- addition, subtraction, multiplication, division
+- comparison
+- floor, ceil, abs
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use num_traits::One;
 use std::{cmp::Ordering, ops};
 
 #[derive(Debug, Clone)]
-pub enum APlusBSqrtN {
+enum APlusBSqrtN_ {
     Rational(BigFraction),
     Irrational {
         a: BigFraction,
@@ -13,6 +13,9 @@ pub enum APlusBSqrtN {
         n: BigUint,     // non-perfect-square
     },
 }
+
+#[derive(Debug, Clone)]
+pub struct APlusBSqrtN(APlusBSqrtN_);
 
 #[must_use]
 fn is_perfect_square(n: &BigUint) -> bool {
@@ -31,9 +34,9 @@ impl APlusBSqrtN {
         );
 
         if b == BigFraction::zero() || is_perfect_square(&n) {
-            Self::Rational(a + b * n.sqrt())
+            Self(APlusBSqrtN_::Rational(a + b * n.sqrt()))
         } else {
-            Self::Irrational { a, b, n }
+            Self(APlusBSqrtN_::Irrational { a, b, n })
         }
     }
 
@@ -45,28 +48,28 @@ impl APlusBSqrtN {
             !(a.is_nan() || a.is_infinite()),
             "Cannot have either Infinity or NaN in `a` or `b`"
         );
-        Self::Rational(a)
+        Self(APlusBSqrtN_::Rational(a))
     }
 
     #[must_use]
     pub const fn is_rational(&self) -> bool {
-        matches!(self, APlusBSqrtN::Rational(_))
+        matches!(self, APlusBSqrtN(APlusBSqrtN_::Rational(_)))
     }
 }
 
 impl PartialEq for APlusBSqrtN {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (APlusBSqrtN::Rational(a1), APlusBSqrtN::Rational(a2)) => a1 == a2,
-            (APlusBSqrtN::Rational(_), APlusBSqrtN::Irrational { .. })
-            | (APlusBSqrtN::Irrational { .. }, APlusBSqrtN::Rational(_)) => false,
+        match (&self.0, &other.0) {
+            (APlusBSqrtN_::Rational(a1), APlusBSqrtN_::Rational(a2)) => a1 == a2,
+            (APlusBSqrtN_::Rational(_), APlusBSqrtN_::Irrational { .. })
+            | (APlusBSqrtN_::Irrational { .. }, APlusBSqrtN_::Rational(_)) => false,
             (
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a1,
                     b: b1,
                     n: n1,
                 },
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a2,
                     b: b2,
                     n: n2,
@@ -87,9 +90,9 @@ impl ops::Neg for APlusBSqrtN {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
-        match self {
-            APlusBSqrtN::Rational(a) => Self::Rational(-a),
-            APlusBSqrtN::Irrational { a, b, n } => Self::new(-a, -b, n),
+        match self.0 {
+            APlusBSqrtN_::Rational(a) => Self(APlusBSqrtN_::Rational(-a)),
+            APlusBSqrtN_::Irrational { a, b, n } => Self::new(-a, -b, n),
         }
     }
 }
@@ -97,20 +100,22 @@ impl ops::Neg for APlusBSqrtN {
 impl ops::Add for APlusBSqrtN {
     type Output = Self;
     fn add(self, other: Self) -> Self::Output {
-        match (self, other) {
-            (APlusBSqrtN::Rational(a1), APlusBSqrtN::Rational(a2)) => Self::Rational(a1 + a2),
-            (APlusBSqrtN::Irrational { a, b, n }, APlusBSqrtN::Rational(a2))
-            | (APlusBSqrtN::Rational(a2), APlusBSqrtN::Irrational { a, b, n }) => {
+        match (self.0, other.0) {
+            (APlusBSqrtN_::Rational(a1), APlusBSqrtN_::Rational(a2)) => {
+                Self(APlusBSqrtN_::Rational(a1 + a2))
+            }
+            (APlusBSqrtN_::Irrational { a, b, n }, APlusBSqrtN_::Rational(a2))
+            | (APlusBSqrtN_::Rational(a2), APlusBSqrtN_::Irrational { a, b, n }) => {
                 Self::new(a + a2, b, n)
             }
 
             (
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a1,
                     b: b1,
                     n: n1,
                 },
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a2,
                     b: b2,
                     n: n2,
@@ -154,19 +159,19 @@ impl ops::Mul for APlusBSqrtN {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        match (self, rhs) {
-            (APlusBSqrtN::Rational(a1), APlusBSqrtN::Rational(a2)) => Self::Rational(a1 * a2),
-            (APlusBSqrtN::Rational(multiplier), APlusBSqrtN::Irrational { a, b, n })
-            | (APlusBSqrtN::Irrational { a, b, n }, APlusBSqrtN::Rational(multiplier)) => {
+        match (self.0, rhs.0) {
+            (APlusBSqrtN_::Rational(a1), APlusBSqrtN_::Rational(a2)) => Self::from_rational(a1 * a2),
+            (APlusBSqrtN_::Rational(multiplier), APlusBSqrtN_::Irrational { a, b, n })
+            | (APlusBSqrtN_::Irrational { a, b, n }, APlusBSqrtN_::Rational(multiplier)) => {
                 Self::new(a * multiplier.clone(), b * multiplier, n)
             }
             (
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a1,
                     b: b1,
                     n: n1,
                 },
-                APlusBSqrtN::Irrational {
+                APlusBSqrtN_::Irrational {
                     a: a2,
                     b: b2,
                     n: n2,
@@ -211,9 +216,9 @@ impl ops::Mul for APlusBSqrtN {
 impl APlusBSqrtN {
     #[must_use]
     pub fn recip(self) -> Self {
-        match self {
-            APlusBSqrtN::Rational(a) => Self::from_rational(GenericFraction::one() / a), /* has to check that this does not lead to infinity */
-            APlusBSqrtN::Irrational { a, b, n } => {
+        match self.0 {
+            APlusBSqrtN_::Rational(a) => Self::from_rational(GenericFraction::one() / a), /* has to check that this does not lead to infinity */
+            APlusBSqrtN_::Irrational { a, b, n } => {
                 // ans = 1 / [a + b * sqrt(n)]
                 //     = [a - b * sqrt(n)] / [a^2 - b^2 * n]
                 let dividing_factor = a.clone() * a.clone() - b.clone() * b.clone() * n.clone();
@@ -235,9 +240,9 @@ impl ops::Div for APlusBSqrtN {
 impl APlusBSqrtN {
     #[must_use]
     pub fn is_positive(&self) -> bool {
-        match self {
-            APlusBSqrtN::Rational(a) => a > &BigFraction::zero(),
-            APlusBSqrtN::Irrational { a, b, n } => {
+        match &self.0 {
+            APlusBSqrtN_::Rational(a) => a > &BigFraction::zero(),
+            APlusBSqrtN_::Irrational { a, b, n } => {
                 if b > &BigFraction::zero() {
                     if a >= &BigFraction::zero() {
                         return true;
@@ -298,9 +303,9 @@ impl std::cmp::PartialOrd for APlusBSqrtN {
 impl APlusBSqrtN {
     #[must_use]
     pub fn floor(self) -> GenericFraction<BigUint> {
-        match &self {
-            APlusBSqrtN::Rational(a) => a.floor(),
-            APlusBSqrtN::Irrational { a, b, n } => {
+        match &self.0 {
+            APlusBSqrtN_::Rational(a) => a.floor(),
+            APlusBSqrtN_::Irrational { a, b, n } => {
                 let b2n = b.clone() * b.clone() * n.clone();
 
                 // m is an integer such that the only possible values for Self::floor() are m-1, m and m+1.
@@ -336,18 +341,18 @@ impl APlusBSqrtN {
                     a.floor()
                 };
 
-                if Self::Rational(m.clone() + GenericFraction::one()) <= self.clone() {
+                if Self::from_rational(m.clone() + GenericFraction::one()) <= self.clone() {
                     if self.clone()
-                        >= Self::Rational(
+                        >= Self::from_rational(
                             m.clone() + GenericFraction::one() + GenericFraction::one(),
                         )
                     {
                         unreachable!("oh no! there is a bug and actually self >= m+2")
                     }
                     return m + GenericFraction::one();
-                } else if Self::Rational(m.clone()) <= self.clone() {
+                } else if Self::from_rational(m.clone()) <= self.clone() {
                     return m;
-                } else if Self::Rational(m.clone() - GenericFraction::one()) <= self.clone() {
+                } else if Self::from_rational(m.clone() - GenericFraction::one()) <= self.clone() {
                     return m - GenericFraction::one();
                 }
                 unreachable!("oh no! there is a bug and actually self < m-1")
@@ -363,7 +368,7 @@ impl APlusBSqrtN {
 
 impl num_traits::identities::Zero for APlusBSqrtN {
     fn zero() -> Self {
-        Self::Rational(GenericFraction::zero())
+        Self(APlusBSqrtN_::Rational(GenericFraction::zero()))
     }
 
     fn is_zero(&self) -> bool {
@@ -373,7 +378,7 @@ impl num_traits::identities::Zero for APlusBSqrtN {
 
 impl num_traits::identities::One for APlusBSqrtN {
     fn one() -> Self {
-        Self::Rational(GenericFraction::one())
+        Self(APlusBSqrtN_::Rational(GenericFraction::one()))
     }
 }
 
